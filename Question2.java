28b39a77a8f8938f1b4d556882f8478fe0c2b9b6
Skyla LaValley <skyla.lavalley@daviestech.org>
2020-10-05 15:31:52 +0000

    Final :)

--- Question2.java
+++ Question2.java
@@ -1,11 +1,24 @@
 /* Activity 1.2 - Question 2 */
-
+import java.util.Scanner;
 
 
 class Question2 {
     public static void main(String[] args) {
 
         /* Write your code here */
+        Scanner scan = new Scanner(System.in); 
+        String name1;
+        String name2;
+        String name3;
+
+        System.out.println("Please enter three names (Enter after each name): ");
+        name1 = scan.nextLine();
+        System.out.println(" ");
+        name2 = scan.nextLine();
+        System.out.println(" ");
+        name3 = scan.nextLine();
+        
+        System.out.println(name3 + " " + name2+ " " + name1);
 
     }
 }
--- Question3.java
+++ Question3.java
@@ -1,11 +1,18 @@
 /* Activity 1.2 - Question 3 */
-
+import java.util.Scanner;
 
 
 class Question3 {
     public static void main(String[] args) {
 
         /* Write your code here */
-
+        Scanner scan = new Scanner(System.in);
+        String name;
+        String adj;
+         System.out.println("Hello. What is your name?");
+         name = scan.nextLine();
+         System.out.println("What adjective describes you?");
+         adj = scan.nextLine();
+         System.out.println("My name is " + name + ". I am " + adj );
     }
 }
